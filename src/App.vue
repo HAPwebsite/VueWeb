e112f3ce2fda81513874432e3b3fda5579e8045a
Erblin Derguti <erblinderguti@gmail.com>
2018-05-15 14:18:07 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Login/>
+    <Navbar/>
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+
+export default {
+  name: "App",
+  components: {
+    Login,
+    Navbar
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Rubik", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="navbar">
+    <ul>
+      <li>{{ msg }}</li>
+      <li>Blog</li>
+      <li>Contact</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
